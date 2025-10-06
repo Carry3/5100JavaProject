136c10f298e0b3d0e09aa50b2ad868206db5eb5c
Wilmon Chen <2827515505@qq.com>
2025-10-05 23:07:29 -0700

    finish Lab1

--- src/main/java/edu/neu/mgen/App.java
+++ src/main/java/edu/neu/mgen/App.java
@@ -1,65 +1,35 @@
 package edu.neu.mgen;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class App {
-  public static boolean canMultiply(int[][] A, int[][] B) {
-    boolean canMultiply = true;
-    int ARowSize = A.length;
-    int AColSize = A[0].length;
-    int BRowSize = B.length;
-    int BColSize = B[0].length;
-    // check column size of A and row size of B are equal
-    if (AColSize != BRowSize) {
-      canMultiply = false;
-    }
-    // check each row of A has the same column size
-    for (int i = 0; i < ARowSize; i++) {
-      if (A[i].length != AColSize) {
-        canMultiply = false;
-      }
-    }
-    // check each row of B has the same column size
-    for (int i = 0; i < BRowSize; i++) {
-      if (B[i].length != BColSize) {
-        canMultiply = false;
-      }
-    }
-    if (!canMultiply) {
-      System.out.println("A and B cannot be multiplied");
-      return false;
-    }
-    return true;
-  }
-
-  public static ArrayList<ArrayList<Integer>> multiply(int[][] A, int[][] B) {
-    if (!canMultiply(A, B)) {
-      return null;
-    }
-    int ARowSize = A.length;
-    int AColSize = A[0].length;
-    int BColSize = B[0].length;
-    ArrayList<ArrayList<Integer>> multipliedResult = new ArrayList<>();
-    for (int i = 0; i < ARowSize; i++) {
-      ArrayList<Integer> row = new ArrayList<>();
-      for (int j = 0; j < BColSize; j++) {
-        int sum = 0;
-        for (int k = 0; k < AColSize; k++) {
-          sum += A[i][k] * B[k][j];
-        }
-        row.add(sum);
-      }
-      multipliedResult.add(row);
-    }
-    return multipliedResult;
-  }
-
   public static void main(String[] args) {
-    int[][] A = { { 2, 3, 4 }, { 3, 4, 5 } }, B = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
-    if (!canMultiply(A, B)) {
-      return;
+    // Part1
+    System.out.println("Lab1 Part1:");
+    int[] x = { 3, 9, 4, 16, 25 }, y = { 5, 10, 15, 20, 25 };
+    int[] z = new int[5];
+    for (int i = 0; i < 5; i++) {
+      z[i] = Math.max(x[i], y[i]);
     }
-    ArrayList<ArrayList<Integer>> multipliedResult = multiply(A, B);
-    System.out.println(multipliedResult);
+    System.out.println("Array x = " + Arrays.toString(x).replace("[", "{").replace("]", "}"));
+    System.out.println("Array y = " + Arrays.toString(y).replace("[", "{").replace("]", "}"));
+    System.out.println("Array z = max(x, y) = " + Arrays.toString(z).replace("[", "{").replace("]", "}"));
+
+    // Part2
+    System.out.println("Lab1 Part2:");
+    ArrayList<String> names = new ArrayList<>();
+    names.add("Weiming");
+    names.add("Jack");
+    names.add("John");
+    names.add("Avril");
+    names.add("William");
+    ArrayList<String> switchedNames = new ArrayList<>();
+    for (String name : names) {
+      switchedNames.add(name.substring(name.length() - 1).toUpperCase() + name.substring(1, name.length() - 1)
+          + name.substring(0, 1).toLowerCase());
+    }
+    System.out.println("Names = " + names.toString().replace("[", "{").replace("]", "}"));
+    System.out.println("Names (switched) = " + switchedNames.toString().replace("[", "{").replace("]", "}"));
   }
 }
