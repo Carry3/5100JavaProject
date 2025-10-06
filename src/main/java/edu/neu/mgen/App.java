c57978d00428b682929b0eec09cba8c182017a29
Wilmon Chen <2827515505@qq.com>
2025-10-05 23:22:13 -0700

    finish HW8

--- src/main/java/edu/neu/mgen/App.java
+++ src/main/java/edu/neu/mgen/App.java
@@ -1,35 +1,31 @@
 package edu.neu.mgen;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class App {
-  public static void main(String[] args) {
-    // Part1
-    System.out.println("Lab1 Part1:");
-    int[] x = { 3, 9, 4, 16, 25 }, y = { 5, 10, 15, 20, 25 };
-    int[] z = new int[5];
-    for (int i = 0; i < 5; i++) {
-      z[i] = Math.max(x[i], y[i]);
-    }
-    System.out.println("Array x = " + Arrays.toString(x).replace("[", "{").replace("]", "}"));
-    System.out.println("Array y = " + Arrays.toString(y).replace("[", "{").replace("]", "}"));
-    System.out.println("Array z = max(x, y) = " + Arrays.toString(z).replace("[", "{").replace("]", "}"));
+  public static String reverseString(String str) {
+    return new StringBuilder(str).reverse().toString();
+  }
 
-    // Part2
-    System.out.println("Lab1 Part2:");
-    ArrayList<String> names = new ArrayList<>();
-    names.add("Weiming");
-    names.add("Jack");
-    names.add("John");
-    names.add("Avril");
-    names.add("William");
-    ArrayList<String> switchedNames = new ArrayList<>();
-    for (String name : names) {
-      switchedNames.add(name.substring(name.length() - 1).toUpperCase() + name.substring(1, name.length() - 1)
-          + name.substring(0, 1).toLowerCase());
+  public static String[] reverseNames(String[] names) {
+    String[] reversedNames = new String[names.length];
+    for (int i = 0; i < names.length; i++) {
+      // reverse the string
+      String reversedString = reverseString(names[i].toLowerCase());
+      // capitalize the first letter
+      reversedNames[i] = reversedString.substring(0, 1).toUpperCase() + reversedString.substring(1);
     }
-    System.out.println("Names = " + names.toString().replace("[", "{").replace("]", "}"));
-    System.out.println("Names (switched) = " + switchedNames.toString().replace("[", "{").replace("]", "}"));
+    return reversedNames;
+  }
+
+  public static void main(String[] args) {
+    String[] names = { "Anne", "John", "Alex", "Jessica" };
+    String[] reversedNames = reverseNames(names);
+    System.out.println("Names = " + Arrays.toString(names).replace("[", "{").replace("]", "}"));
+    System.out.println("Reversed Names = " + Arrays.toString(reversedNames).replace("[", "{").replace("]", "}"));
+    names = new String[] { "Sun", "Mercury", "Venis", "Earth", "Mars", "Jupiter" };
+    reversedNames = reverseNames(names);
+    System.out.println("Names = " + Arrays.toString(names).replace("[", "{").replace("]", "}"));
+    System.out.println("Reversed Names = " + Arrays.toString(reversedNames).replace("[", "{").replace("]", "}"));
   }
 }
