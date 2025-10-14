aac36efd6c9c0178028eeaea77c3f63eff7e41bc
Wilmon Chen <2827515505@qq.com>
2025-10-13 22:50:21 -0700

    finish HW9

--- src/main/java/edu/neu/mgen/App.java
+++ src/main/java/edu/neu/mgen/App.java
@@ -1,35 +1,12 @@
 package edu.neu.mgen;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class App {
-  public static void main(String[] args) {
-    // Part1
-    System.out.println("Lab1 Part1:");
-    int[] x = { 3, 9, 4, 16, 25 }, y = { 5, 10, 15, 20, 25 };
-    int[] z = new int[5];
-    for (int i = 0; i < 5; i++) {
-      z[i] = Math.max(x[i], y[i]);
+    public static void main(String[] args) {
+        EngClass engClass = new EngClass();
+        engClass.students.add(new Student(1, "Weiming", "Chen"));
+        engClass.students.add(new Student(2, "Jack", "Oaa"));
+        engClass.printStudents();
+        engClass.removeStudent(1);
+        engClass.printStudents();
     }
-    System.out.println("Array x = " + Arrays.toString(x).replace("[", "{").replace("]", "}"));
-    System.out.println("Array y = " + Arrays.toString(y).replace("[", "{").replace("]", "}"));
-    System.out.println("Array z = max(x, y) = " + Arrays.toString(z).replace("[", "{").replace("]", "}"));
-
-    // Part2
-    System.out.println("Lab1 Part2:");
-    ArrayList<String> names = new ArrayList<>();
-    names.add("Weiming");
-    names.add("Jack");
-    names.add("John");
-    names.add("Avril");
-    names.add("William");
-    ArrayList<String> switchedNames = new ArrayList<>();
-    for (String name : names) {
-      switchedNames.add(name.substring(name.length() - 1).toUpperCase() + name.substring(1, name.length() - 1)
-          + name.substring(0, 1).toLowerCase());
-    }
-    System.out.println("Names = " + names.toString().replace("[", "{").replace("]", "}"));
-    System.out.println("Names (switched) = " + switchedNames.toString().replace("[", "{").replace("]", "}"));
-  }
 }
---
+++ src/main/java/edu/neu/mgen/Student.java
@@ -0,0 +1,13 @@
+package edu.neu.mgen;
+
+public class Student {
+    int studentId;
+    String nameFirst;
+    String nameLast;
+
+    public Student(int studentId, String nameFirst, String nameLast) {
+        this.studentId = studentId;
+        this.nameFirst = nameFirst;
+        this.nameLast = nameLast;
+    }
+}
